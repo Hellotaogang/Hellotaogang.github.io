8ce3a55e9d314aae095c406d248abdb017e99a3f
taogang <cdtaogang@163.com>
2019-10-14 16:15:14 +0800

    Site updated: 2019-10-14 16:15:09

--- js/click_show_text.js
+++ js/click_show_text.js
@@ -1,8 +1,18 @@
 var a_idx = 0;
 jQuery(document).ready(function($) {
     $("body").click(function(e) {
+        //文字颜色
+        var colorArr = ['red','yellow','green','blue','orange','black','pink','Violet','Cyan']
+        //随机取出颜色
+        var colorIndex = Math.floor((Math.random()*colorArr.length))
+        var color = colorArr[colorIndex]
+        function getRandomColor(){
+        var rgb='rgb('+Math.floor(Math.random()*255)+','
+                +Math.floor(Math.random()*255)+','
+                +Math.floor(Math.random()*255)+')';
+        return rgb;}
         var a = new Array
-        ("富强", "民主", "文明", "和谐", "自由", "平等", "公正", "法治", "爱国", "敬业", "诚信", "友善");
+        ("❤富强❤","❤民主❤","❤文明❤","❤和谐❤","❤自由❤","❤平等❤","❤公正❤","❤法治❤","❤爱国❤","❤敬业❤","❤诚信❤","❤友善❤");
         var $i = $("<span/>").text(a[a_idx]);
         a_idx = (a_idx + 1) % a.length;
         var x = e.pageX,
@@ -13,7 +23,7 @@ jQuery(document).ready(function($) {
             "left": x,
             "position": "absolute",
             "font-weight": "bold",
-            "color": "#FF0000"
+            "color": getRandomColor()
         });
         $("body").append($i);
         $i.animate({
